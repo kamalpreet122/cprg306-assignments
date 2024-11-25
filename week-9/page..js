66857ec8a4caa4943af8691c8f4d69a619952d89
Kamalpreet Kaur <167359302+kamalpreet122@users.noreply.github.com>
2024-11-25 12:39:27 -0700

    Add files via upload

---
+++ week-9/page..js
@@ -0,0 +1,36 @@
+"use client";
+
+import { useUserAuth } from "./_utils/auth-context";
+import SecurePage from "./shopping-list/page";
+
+export default function Page() {
+    const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
+
+    const login = async () => {
+        await gitHubSignIn();
+    };
+
+    const logout = async () => {
+        await firebaseSignOut();
+    };
+
+    return (
+        <div>
+            <header>
+                <nav>
+                    {user ? (
+                        <div className="p-2 bg-neutral-900 ">
+                            <div className="flex place-items-center justify-between ">
+                                <span className="text-neutral-100 text-5xl" >You are logged in as, {user.displayName}</span>
+                                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded " onClick={firebaseSignOut} >Sign Out</button>
+                            </div>
+                            <SecurePage />
+                        </div>
+                    ) : (
+                        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={gitHubSignIn}>Sign In</button>
+                    )}
+                </nav>
+            </header>
+        </div>
+    );
+}
